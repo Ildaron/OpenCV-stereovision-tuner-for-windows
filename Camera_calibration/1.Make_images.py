d4b8dd88f5547e52a8a5435590c1d1f47a833e02
Ildar <de.ikar@yandex.ru>
2021-04-02 10:39:02 +0900

    Update 1.Make_images.py

--- Camera_calibration/1.Make_images.py
+++ Camera_calibration/1.Make_images.py
@@ -11,8 +11,8 @@ for a in range (0,30,1):
  cap2 = cv2.VideoCapture(2)         
  ret, frame1 = cap1.read()
  ret, frame2 = cap2.read()
- cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/right/'+'img'+str(a)+'.png',frame1)
- cv2.imwrite('C:/Users/rakhmatulin/Desktop/jre_new/2021/4.Stereo_vision/programme/left/'+'img'+str(a)+'.png',frame2)
+ cv2.imwrite('C:/Users/right/'+'img'+str(a)+'.png',frame1)
+ cv2.imwrite('C:/Users/left/'+'img'+str(a)+'.png',frame2)
 cv2.destroyAllWindows()
 
 
